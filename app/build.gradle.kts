cdb85cd35b8a73fbfbb68774d423fb0832238771
Nikita Obrekht <obrekht@gmail.com>
2023-10-07 20:10:22 +0200

    Add more libraries Coroutines Serialization Navigation Room Retrofit Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,27 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
+    implementation(libs.coroutines)
+    implementation(libs.serialization)
+    implementation(libs.jetpack.core)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.fragment)
     implementation(libs.constraintlayout)
+    implementation(libs.bundles.navigation)
+
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
+
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+    implementation(libs.bundles.retrofit)
+
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+    ksp(libs.jetpack.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
